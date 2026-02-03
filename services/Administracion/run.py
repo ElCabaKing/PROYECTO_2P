8001ab2c6bad306930136409535d2851f9a74828
Diegopalma123 <diegito.palma1245@gmail.com>
2026-02-03 03:15:41 +0000

    arreglando los logs del  handleLogs

--- services/Administracion/run.py
+++ services/Administracion/run.py
@@ -1,12 +1,14 @@
-from src.app import create_app
+from app import create_app
+from src.core.logs import HandleLogs
 from dotenv import load_dotenv
+
 load_dotenv()
 app = create_app()
 
 if __name__ == '__main__':
     try:
-      HandleLogs.write_log("Servicio Iniciado")
-      src.run(host='0.0.0.0', port=5001, debug=True, threaded=True)
+        HandleLogs.write_log("Servicio Iniciado")
+        app.run(host='0.0.0.0', port=5001, debug=True, threaded=True)
     except Exception as err:
         HandleLogs.write_error(err)
     finally:
